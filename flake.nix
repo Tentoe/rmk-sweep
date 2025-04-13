2f672949c8b817c8c0f9bdd424c9d2fbca6052a0
Tentoe <tentoe86@pm.me>
2025-04-13 13:28:02 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.thumbv7em-none-eabihf.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+
+      in
+      {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            rust-analyzer
+            lldb # debugger
+            rustfmt
+            flip-link # zero-cost stack overflow protection.
+            cargo-make # used to automate uf2 generation.
+            probe-rs-tools # arm debugging and flashing
+          ];
+          shellHook = ''
+            export SHELL="fish"
+          '';
+        };
+      }
+    );
+}
